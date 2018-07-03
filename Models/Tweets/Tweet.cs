1c106be6f9745a4cf3d674509bae270d59e370a4
Bibesh KC <bibesh.kc@selu.edu>
2018-07-02 20:35:56 -0500

    added a class library with User and Tweet Class

---
+++ Models/Tweets/Tweet.cs
@@ -0,0 +1,21 @@
+﻿using Models.Core.Features.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ClassLibray.Core.Features.Tweets
+{
+    public class Tweet
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
---
+++ Models/Users/User.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibray.Core.Features.Tweets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.Core.Features.Users
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Bio { get; set; }
+
+        public string Website { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+        public ICollection<Tweet> Tweets { get; set; }
+    }
+}
