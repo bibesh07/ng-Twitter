0dfc1f3e76355faeef6fa7db5624be0fb62def16
Amrit Subedi <amrit.subedi@selu.edu>
2018-07-06 23:22:23 -0500

    Fixed issue with dynamic Connection string
    Co-Authored-By: Tika Pahadi <tika.pahadi@selu.edu>

--- ng-Twitter/Data/DataContext.cs
+++ ng-Twitter/Data/DataContext.cs
@@ -4,8 +4,10 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Models.Core.Features.Users;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 public class DataContext : DbContext
 {
@@ -23,9 +25,20 @@ public class DataContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        if(!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TwitterClone;Trusted_Connection=True");
-        }
+        if (optionsBuilder.IsConfigured) return;
+        
+        // Build Configuration:
+        var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .Build();
+
+        // Check if configuration is good:
+        if (string.IsNullOrEmpty(configuration.GetConnectionString("DefaultConnection")))
+            throw new Exception("Connection string (default) not set. Please set them on Repository or Web Project.");
+
+        Console.WriteLine("Using Connection string: {0}",configuration.GetConnectionString("(default)"));
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        
     }
 }
