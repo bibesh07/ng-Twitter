c69167ea89db50357110905de801d709cd4a7f48
Amrit Subedi <amrit.subedi@selu.edu>
2018-07-07 15:55:05 -0500

    Search By Id and Update password

--- ng-Twitter/Services/UserService.cs
+++ ng-Twitter/Services/UserService.cs
@@ -16,6 +16,17 @@ namespace ng_Twitter.Services
 
         public User GetUserByEmail(string Email) => _context.Users.Include(t => t.Tweets).First(m => m.Email == Email);
 
+        public User GetUserById(int id) => _context.Users.Include(t => t.Tweets).First(i => i.Id == id);
+
         public ICollection<User> GetAllUsers() => _context.Users.Include(t => t.Tweets).ToList();
+        
+        public void UpdatePasswordByUserId(int id, string password)
+        {
+            var user = _context.Users.Find(id);
+            user.Password = password;
+            _context.Update(user);
+            _context.SaveChanges();
+        }
+        
     }
 }
